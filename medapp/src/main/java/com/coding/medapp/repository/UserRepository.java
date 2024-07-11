586c502c75f90cbe272f60c7cae426d9aa008baf
Paloma Jellinek <palomacaviezel7@gmail.com>
2024-07-11 18:11:12 -0300

    correcciones

--- medapp/src/main/java/com/coding/medapp/repository/UserRepository.java
+++ medapp/src/main/java/com/coding/medapp/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.coding.medapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
--- medapp/src/main/java/com/coding/medapp/services/UserServices.java
+++ medapp/src/main/java/com/coding/medapp/services/UserServices.java
@@ -1,8 +1,11 @@
 package com.coding.medapp.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.coding.medapp.models.User;
 import com.coding.medapp.repository.UserRepository;
 
 @Service
