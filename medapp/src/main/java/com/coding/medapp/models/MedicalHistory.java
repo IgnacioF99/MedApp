5bbf709607ca4beda2d61c9c79c435f4139ef63c
Ignacio Gabriel Flores <chinitochitoo@gmail.com>
2024-07-10 22:41:32 -0300

    Entidades Content, MedicalHistories, User

---
+++ medapp/src/main/java/com/coding/medapp/models/MedicalHistory.java
@@ -0,0 +1,72 @@
+package com.coding.medapp.models;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "medical_records")
+public class MedicalHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+
+    //=========================================================
+
+    public MedicalHistory() {
+    }
+
+    //=========================================================
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    //=========================================================
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+    
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
+}
--- medapp/src/main/java/com/coding/medapp/models/User.java
+++ medapp/src/main/java/com/coding/medapp/models/User.java
@@ -1,13 +1,161 @@
 package com.coding.medapp.models;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotEmpty(message = "First name is required.")
+    @Size(min = 2, message = "First name needs at least 2 chars")
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, message = "Last name needs at least 2 chars")
+    private String lastName;
+
+    @NotEmpty(message = "DNI is required.")
+    @Size(min = 7, max = 8, message = "DNI invalid")
+    private int dni;
+
+    @NotEmpty
+    @Size(min = 2, message = "location needs at least 2 chars")
+    private String location;
+
+    @NotEmpty(message = "Email is required.")
+    @Email(message = "Invalid email") //Validar que sea un correo electronico valido
+    private String email;
+
+    @NotEmpty(message = "Password is required.")
+    @Size(min = 6, message = "Password needs at least 6 chars")
+    private String password;
+
+    @Transient //No se guarde el dato en la base de datos
+    @NotEmpty(message = "Password is required.")
+    @Size(min = 6, message = "Password needs at least 6 chars")
+    private String confirm;
+
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+
+    //=========================================================
+
+    public User() {
+    }
+
+    //=========================================================
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirm() {
+        return confirm;
+    }
+
+    public void setConfirm(String confirm) {
+        this.confirm = confirm;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    //=========================================================
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+    
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
