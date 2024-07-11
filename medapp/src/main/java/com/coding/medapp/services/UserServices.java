ea4a93e1acfc1b448f7019fe2c3c022834546341
IgnacioF99 <127629756+IgnacioF99@users.noreply.github.com>
2024-07-11 17:55:46 -0300

    Revert "findAll en service y repo User"

--- medapp/src/main/java/com/coding/medapp/services/UserServices.java
+++ medapp/src/main/java/com/coding/medapp/services/UserServices.java
@@ -1,11 +1,8 @@
 package com.coding.medapp.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.coding.medapp.models.User;
 import com.coding.medapp.repository.UserRepository;
 
 @Service
@@ -13,7 +10,4 @@ public class UserServices {
     @Autowired
     private UserRepository userRepository;
     
-    public List<User> findAllUsers() {
-        return userRepository.findAll();
-    }
 }
