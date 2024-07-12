c18c733f57c09de6ab26f1b58aa6ecf46cd45829
Martin Pereira <martinpereira794@gmail.com>
2024-07-12 19:56:37 -0300

    Servicios de Especialidades, Historial Medico y Login and Register de User

--- medapp/src/main/java/com/coding/medapp/repository/UserRepository.java
+++ medapp/src/main/java/com/coding/medapp/repository/UserRepository.java
@@ -13,5 +13,5 @@ public interface UserRepository extends CrudRepository<User,Long>{
 
 	List<User> findAll();   
 
-
+	User findByEmail(String email);
 }
--- medapp/src/main/java/com/coding/medapp/services/SpecialityServices.java
+++ medapp/src/main/java/com/coding/medapp/services/SpecialityServices.java
@@ -1,13 +1,27 @@
 package com.coding.medapp.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.coding.medapp.models.Speciality;
 import com.coding.medapp.repository.SpecialityRepository;
 
 @Service
 public class SpecialityServices {
-    @Autowired
+    
+	@Autowired
     private SpecialityRepository specialityRepository;
+    
+    public List<Speciality> findAllSpecialties(){
+    	return specialityRepository.findAll();
+    }
+    
+    public Speciality getSpeciality(Long id) {
+    	return specialityRepository.findById(id).orElse(null);
+    }
+    
+    
 
 }
